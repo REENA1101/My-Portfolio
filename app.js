ab81f65e7e238099df68c04afdbf78044d6955ac
Reena kumari <reenakvs1@gmail.com>
2025-08-01 14:57:21 +0530

    styling changing section

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const allSections = document.querySelector('.main-content');
 
 
 function pageTransitions(){
+    // button click and adds the active class
      for(let i=0; i < sectBtn.length;i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
@@ -12,6 +13,27 @@ function pageTransitions(){
             this.className += ' active-btn'
         })
      }
+
+
+// Active section sswitching up function 
+allSections.addEventListener('click', (e)=>{
+      const id = e.target.dataset.id;
+      if(id){
+        sectBtns.forEach((btn)=>{
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        // hide other sections 
+        sections.forEach((section)=>{
+            section.classList.remove(active)
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+      }
+})
+
 }
 
 pageTransitions();
